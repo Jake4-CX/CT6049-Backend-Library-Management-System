4a40837764708dcbb8068d13724cb74a5ff02062
= <jake44264@hotmail.co.uk>
2023-11-24 20:12:01 +0000

    feat: updated MongoDB search endpoint format

--- src/main/java/me/jack/lat/lmsbackendmongo/resources/books/SearchBooksResource.java
+++ src/main/java/me/jack/lat/lmsbackendmongo/resources/books/SearchBooksResource.java
@@ -5,9 +5,12 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import me.jack.lat.lmsbackendmongo.annotations.UnprotectedRoute;
 import me.jack.lat.lmsbackendmongo.entities.Book;
+import me.jack.lat.lmsbackendmongo.entities.LoanedBook;
 import me.jack.lat.lmsbackendmongo.enums.DatabaseTypeEnum;
 import me.jack.lat.lmsbackendmongo.service.mongoDB.BookService;
+import me.jack.lat.lmsbackendmongo.service.mongoDB.LoanedBookService;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -35,13 +38,24 @@ public class SearchBooksResource {
     }
 
     public Response searchBooksMongoDB(String bookName) {
-
-        BookService bookService = new BookService();
-        List<Book> bookResults = bookService.searchBooks(bookName);
-
         Map<String, Object> response = new HashMap<>();
 
-        response.put("books", bookResults);
+        BookService bookService = new BookService();
+        LoanedBookService loanedBookService = new LoanedBookService();
+
+        List<Book> bookResults = bookService.searchBooks(bookName);
+        List<Object> booksObject = new ArrayList<>();
+
+        bookResults.forEach((book) -> {
+            List<LoanedBook> loanedBooks = loanedBookService.getUnreturnedBooksWithBook(book);
+            Map<String, Object> bookObject = new HashMap<>();
+            bookObject.put("book", book);
+            bookObject.put("booksLoaned", loanedBooks.size());
+
+            booksObject.add(bookObject);
+        });
+
+        response.put("books", booksObject);
 
         return Response.status(Response.Status.OK).entity(response).type(MediaType.APPLICATION_JSON).build();
     }
