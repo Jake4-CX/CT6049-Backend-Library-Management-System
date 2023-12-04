fcc7f082872f94512708e4b0c3bb06e2473e6258
= <jake44264@hotmail.co.uk>
2023-12-04 23:59:14 +0000

    feat: get admin book circulation statistics function (MongoDB)

--- src/main/java/me/jack/lat/lmsbackendmongo/service/mongoDB/StatisticsService.java
+++ src/main/java/me/jack/lat/lmsbackendmongo/service/mongoDB/StatisticsService.java
@@ -1,11 +1,17 @@
 package me.jack.lat.lmsbackendmongo.service.mongoDB;
 
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Aggregates;
 import dev.morphia.Datastore;
 import dev.morphia.query.filters.Filters;
+import me.jack.lat.lmsbackendmongo.entities.Book;
 import me.jack.lat.lmsbackendmongo.entities.LoanedBook;
 import me.jack.lat.lmsbackendmongo.util.MongoDBUtil;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class StatisticsService {
@@ -42,5 +48,33 @@ public class StatisticsService {
         return stats;
     }
 
+    /**
+        Admin Book Circulation Statistics, returns totalIssuedBooks, totalOverdueBooks, totalNotOverdueBooks
+
+        @return HashMap<String, Object>
+
+     **/
+    public HashMap<String, Object> getAdminBookCirculationStatistics() {
+
+        HashMap<String, Object> stats = new HashMap<>();
+
+        stats.put("totalIssuedBooks", datastore.find(LoanedBook.class).filter(
+                Filters.eq("returnedAt", null)
+        ).count());
+        stats.put("totalOverdueBooks", datastore.find(LoanedBook.class).filter(
+                Filters.and(
+                        Filters.eq("returnedAt", null),
+                        Filters.lte("loanedAt", System.currentTimeMillis() - (14 * 24 * 60 * 60 * 1000))
+                )
+        ).count());
+        stats.put("totalNotOverdueBooks", datastore.find(LoanedBook.class).filter(
+                Filters.and(
+                        Filters.eq("returnedAt", null),
+                        Filters.gt("loanedAt", System.currentTimeMillis() - (14 * 24 * 60 * 60 * 1000))
+                )
+        ).count());
+
+        return stats;
+    }
 
 }
